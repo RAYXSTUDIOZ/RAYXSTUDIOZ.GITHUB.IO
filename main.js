d5e9fbecc3cd66d792fc0bf8eed8878d0ebd0af5
syedrayangames <165355812+syedrayangames@users.noreply.github.com>
2024-06-02 15:08:30 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ document.getElementById('disableAnim').onclick = function() {
   const body = document.getElementById('about');
   body.classList.toggle('disableAnimation');
   console.log(this.innerText);
-  if (this.innerText === 'Disable Animations') {
-    this.innerText = 'Enable Animations';
+  if (this.innerText === 'Enable Animations') {
+    this.innerText = 'Disable Animations';
   } else {
-    this.innerText = 'Disable Animations'
+    this.innerText = 'Enable Animations'
   }
 }
